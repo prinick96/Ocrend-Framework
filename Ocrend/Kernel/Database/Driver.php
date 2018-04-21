4fd1a9101ea91eb174ae168d0aa1f3b24b9ffda5
Brayan Narváez <princk093@gmail.com>
2018-04-20 22:40:16 -0400

    Fix Driver interface

--- Ocrend/Kernel/Database/Driver.php
+++ Ocrend/Kernel/Database/Driver.php
@@ -19,9 +19,9 @@ namespace Ocrend\Kernel\Database;
 interface Driver {
     public function __construct();
     public function scape($param) : string;
-    public function select(string $fields, string $table, $where = null, $limit = null, string $extra = '');
+    public function select(string $fields, string $table, $inners = null, $where = null, $limit = null, string $extra = '');
     public function update(string $table, array $e, $where = null, $limit = null) : int;
     public function insert(string $table, array $e) : int;
-    public function delete(string $table, string $where, $limit = null) : int;
+    public function delete(string $table, $where = null, $limit = null) : int;
     public function __destruct();
 }
